c52fabdec932809895804fb41fc3f6cd8103685b
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-19 01:36:56 +0530

    update sub_todo page

--- mongoose_data_model/models/todos/sub_todo.models.js
+++ mongoose_data_model/models/todos/sub_todo.models.js
@@ -2,6 +2,18 @@ import mongoose from "mongoose";
 
 const subTodoSchema = new mongoose.Schema(
 {
+    content : {
+        type: String,
+        required: true
+    },
+    complete : {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
 
 }, {timestamps: true}
 )
