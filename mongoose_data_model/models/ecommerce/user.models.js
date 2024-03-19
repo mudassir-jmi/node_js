bec67dd33364f683186fed782191332b6c2c5d89
mudassir-jmi <mudassirakhter420@gmail.com>
2024-03-19 14:54:56 +0530

    make user model and category model

--- mongoose_data_model/models/ecommerce/user.models.js
+++ mongoose_data_model/models/ecommerce/user.models.js
@@ -1,5 +1,22 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new mongoose.Schema({},{timestamps: true})
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
